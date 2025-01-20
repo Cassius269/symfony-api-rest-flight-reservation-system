4f0d2a1a132519b3e48c6d304e7d293834e97efa
cassius269 <fahamygaston@gmail.com>
2025-01-20 21:58:47 +0100

    mise en place des opérations HTTP de la ressource Airplane

--- src/Entity/Airplane.php
+++ src/Entity/Airplane.php
@@ -2,14 +2,32 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
 use Doctrine\DBAL\Types\Types;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Delete;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\AirplaneRepository;
+use ApiPlatform\Metadata\GetCollection;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: AirplaneRepository::class)]
-#[ApiResource]
+#[UniqueEntity(
+    fields: ['model'],
+    message: 'Un modèle similaire d\'avion est déjà présente dans le serveur'
+)]
+#[ApiResource(
+    operations: [
+        new Get(), // obtenir une ressource Avion à l'aide de son ID
+        new GetCollection(), // obtenir toutes les ressources de type avion se trouvant dans le serveur
+        new Post(), // créer une nouvelle ressource de type Avion à l'aide de son ID
+        new Delete(), // supprimer une ressource du serveur à l'aide son ID
+        new Patch(), // mettre à jour partiellement une ressource à l'aide de son ID
+    ]
+)]
 class Airplane
 {
     #[ORM\Id]
@@ -26,12 +44,11 @@ class Airplane
     private ?string $model = null;
 
     #[ORM\Column]
-    #[Assert\Positive(message: 'La capacité doit être un nombre supérieur à zéro')]
+    #[Assert\Positive(message: 'La capacité de l\'avion doit être un nombre supérieur à zéro')]
     private ?int $capacity = null;
 
     #[ORM\Column]
     #[Assert\NotBlank(message: 'La date de création de la donnée doit être renseignée')]
-    #[Assert\DateTime('L\'information doit être au format date Y-m-d H:i:s')]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
